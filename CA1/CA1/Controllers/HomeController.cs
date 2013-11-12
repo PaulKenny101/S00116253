e75098f74e0ae6798c6d863927004889c0cbc9f2
S00116253 <S00116253@mail.itsligo.ie>
2013-11-12 00:26:28 +0000

    1st Sub

---
+++ CA1/CA1/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CA1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        MusicStoreDataContext _db = new MusicStoreDataContext();
+        //public ActionResult Index()
+        //{
+        //    var q1 = from o in _db.Orders
+        //             select o;
+        //    return View(q1);
+        //}
+
+        public ActionResult Index(string searchOrder)
+        {
+            var orderByUserName = _db.Orders
+                .Where(or => searchOrder == null || or.Username.Contains(searchOrder));
+            return View(orderByUserName);
+        }
+
+        //
+        // GET: /Home/Details/5
+
+        public ActionResult Details(int id=0)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Home/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
