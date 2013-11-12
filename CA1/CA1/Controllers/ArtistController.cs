3b7a51a4e7e78155461b3e3402f3fef1742ff50c
S00116253 <S00116253@mail.itsligo.ie>
2013-11-12 01:41:12 +0000

    commit to ca1 branch hopefully

---
+++ CA1/CA1/Controllers/ArtistController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CA1.Controllers
+{
+    public class ArtistController : Controller
+    {
+        //
+        // GET: /Artist/
+        MusicStoreDataContext _db = new MusicStoreDataContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Artist/Details/5
+
+        public ActionResult Details(int id)
+        {
+            var x = from a in _db.Artists
+                    join alb in _db.Albums on a.ArtistId equals alb.ArtistId
+                    where alb.AlbumId == id
+                    select a;
+
+            return View(x);
+        }
+
+        //
+        // GET: /Artist/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Artist/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Artist/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Artist/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Artist/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Artist/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CA1/CA1/Controllers/OrderController.cs
@@ -0,0 +1,130 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CA1.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+
+        //create link to local db
+        private MusicStoreDataContext _db = new MusicStoreDataContext();
+
+
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        //sort orders by date
+        public ActionResult IndexByDate()
+        {
+            var x = _db.Orders.OrderBy(a => a.OrderDate);
+            return View(x);
+        }
+
+        //sort orders by size (Cost)
+        public ActionResult IndexBySize()
+        {
+            var x = _db.Orders.OrderByDescending(a => a.Total);
+            return View(x);
+        }
+
+        //public ActionResult Index(string searchOrder)
+        //{
+        //    var orderByUserName = _db.Orders
+        //        .Where(or => searchOrder == null || or.Username.Contains(searchOrder));
+        //    return View(orderByUserName);}
+
+        //
+        // GET: /Order/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Order/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Order/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Order/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Order/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Order/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Order/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
